f20154dbf28f93ec7a9057170626ce785f19b7aa
Miné E <mine_esen@yahoo.ca>
2019-06-01 00:39:12 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,15 @@
 #require 'pry'
 def square_array(array)
+<<<<<<< HEAD
   new_array = []
   array.each do |item|
     new_array.push(item ** 2)
   end
   new_array
+=======
+  array.each do |item|
+    item ** 2
+    array
+  end
+>>>>>>> a597d479a5ca82640cc53bdca1ffc9efb05d30d2
 end
